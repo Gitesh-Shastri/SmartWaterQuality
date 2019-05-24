0fef2c62e0e5c1506f4ce4e693b2eb4b183934f1
apple <giteshshastri96@gmail.com>
2019-05-25 00:17:16 +0530

    Added Api

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ mongoose.connect(MONGODB_URI, function() {
 	console.log('connected to DB');
 });
 
+var admin = require('firebase-admin');
+
+var payload = {
+	data: {
+		MyKey1: 'Hello'
+	}
+};
+
+var serviceAccount = require('./smartwaterquality-d3304-firebase-adminsdk-8avp0-d548da1c59.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://smartwaterquality-d3304.firebaseio.com'
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(
@@ -107,13 +122,21 @@ app.get('/signup', (req, res, next) => {
 });
 
 app.get('/getcontent', (req, res, next) => {
-	Water_quality.find()
+	User.findOne({ useremail: req.query.email })
 		.exec()
-		.then((doc) => {
-			res.status(200).json({ results: doc });
+		.then((user) => {
+			Water_quality.find({ user: user._id })
+				.select('_id created_at temperature Conductivity PH turbidity')
+				.exec()
+				.then((doc) => {
+					res.status(200).json({ results: doc.reverse() });
+				})
+				.catch((err) => {
+					res.status(200).json({ message: 'Error Occured' });
+				});
 		})
 		.catch((err) => {
-			res.redirect('/login');
+			res.status(200).json({ message: 'Error Occured' });
 		});
 });
 
@@ -132,18 +155,79 @@ app.post('/signup', (req, res, next) => {
 		});
 });
 
+app.use('/sendNotification', (req, res, next) => {
+	admin
+		.messaging()
+		.sendToTopic('smart_water', payload)
+		.then(function(response) {
+			console.log('Successfully sent message:', response);
+		})
+		.catch(function(error) {
+			console.log('Error sending message:', error);
+		});
+});
+
+app.use('/a', (req, res, next) => {
+	var water_quality = new Water_quality();
+	water_quality.user = '5ce163407191c40881e32a99';
+	water_quality.temperature = '-';
+	water_quality.Conductivity = Math.floor(Math.random() * (400 - 200) + 200) / 100;
+	water_quality.PH = Math.floor(Math.random() * (130 - 70) + 70) / 10;
+	water_quality.turbidity = Math.floor(Math.random() * (85 - 60) + 60) / 10;
+	water_quality.save();
+	admin
+		.messaging()
+		.sendToTopic('smart_water', payload)
+		.then(function(response) {
+			console.log('Successfully sent message:', response);
+		})
+		.catch(function(error) {
+			console.log('Error sending message:', error);
+		});
+	res.redirect('/');
+});
+
+app.use('/n', (req, res, next) => {
+	var water_quality = new Water_quality();
+	water_quality.user = '5ce163407191c40881e32a99';
+	water_quality.temperature = '-';
+	water_quality.Conductivity = Math.floor(Math.random() * (300 - 110) + 110) / 100;
+	water_quality.PH = Math.floor(Math.random() * (70 - 60) + 60) / 10;
+	water_quality.turbidity = Math.floor(Math.random() * (85 - 60) + 60) / 10;
+	water_quality.save();
+	res.redirect('/');
+});
+
+app.use('/p', (req, res, next) => {
+	var water_quality = new Water_quality();
+	water_quality.user = '5ce163407191c40881e32a99';
+	water_quality.temperature = '-';
+	water_quality.Conductivity = Math.floor(Math.random() * (300 - 110) + 110) / 100;
+	water_quality.PH = Math.floor(Math.random() * (60 - 30) + 30) / 10;
+	water_quality.turbidity = Math.floor(Math.random() * (60 - 30) + 30) / 10;
+	water_quality.save();
+	var payload = {
+		data: {
+			MyKey1: 'Hello'
+		}
+	};
+	admin
+		.messaging()
+		.sendToTopic('smart_water', payload)
+		.then(function(response) {
+			console.log('Successfully sent message:', response);
+		})
+		.catch(function(error) {
+			console.log('Error sending message:', error);
+		});
+	res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-	// var water_quality = new Water_quality();
-	// water_quality.user = '5ce163407191c40881e32a99';
-	// water_quality.temperature = '-';
-	// water_quality.Conductivity = Math.floor(Math.random() * (100 - 50) + 50) / 100;
-	// water_quality.PH = Math.floor(Math.random() * (85 - 60) + 60) / 10;
-	// water_quality.turbidity = '-';
-	// water_quality.save();
 	Water_quality.find()
 		.exec()
 		.then((doc) => {
-			res.render('index', { doc: doc });
+			res.render('index', { doc: doc.reverse() });
 		})
 		.catch((err) => {
 			res.redirect('/login');
